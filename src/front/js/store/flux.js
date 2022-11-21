ef4bf6c05b5770f1b5870d0a392087f41940a243
moniasuka <moniasuka@hotmail.com>
2022-11-21 00:39:18 +0000

    Actualizando vista de categorias y el fetch

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -15,7 +15,7 @@ const getState = ({ getStore, getActions, setStore }) => {
           initial: "white",
         },
       ],
-      categories: [],
+      cat: [],
       person: [],
       planets: [],
       vehicles: [],
@@ -31,18 +31,18 @@ const getState = ({ getStore, getActions, setStore }) => {
         getActions().changeColor(0, "green");
       },
 
-      getMessage: async () => {
-        try {
-          // fetching data from the backend
-          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
-          const data = await resp.json();
-          setStore({ message: data.message });
-          // don't forget to return something, that is how the async resolves
-          return data;
-        } catch (error) {
-          console.log("Error loading message from backend", error);
-        }
-      },
+      // getMessage: async () => {
+      //   try {
+      //     // fetching data from the backend
+      //     const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+      //     const data = await resp.json();
+      //     setStore({ message: data.message });
+      //     // don't forget to return something, that is how the async resolves
+      //     return data;
+      //   } catch (error) {
+      //     console.log("Error loading message from backend", error);
+      //   }
+      // },
       changeColor: (index, color) => {
         //get the store
         const store = getStore();
@@ -97,51 +97,44 @@ const getState = ({ getStore, getActions, setStore }) => {
         let x = aux.filter((element, i) => element.uid != uid);
         setStore({ favorites: x });
       },
-      getCharacter: async (uid) => {
-        let BACKEND_URL = process.env.BACKEND_URL;
-        fetch(`${BACKEND_URL}${uid}`)
-          .then((resp) => resp.json())
-          .then((resp) => setStore({ character: resp.result.properties }))
-          .catch((err) => console.error(err));
-      },
-      getPlanet: async (uid) => {
-        let BACKEND_URL = process.env.BACKEND_URL;
-        fetch(`${BACKEND_URL}${uid}`)
-          .then((resp) => resp.json())
-          .then((resp) => setStore({ planet: resp.result.properties }))
-          .catch((err) => console.error(err));
-      },
-      getSpaceship: async (uid) => {
-        let BACKEND_URL = process.env.BACKEND_URL;
-        fetch(`${BACKEND_URL}${uid}`)
-          .then((resp) => resp.json())
-          .then((resp) => setStore({ vehicle: resp.result.properties }))
-          .catch((err) => console.error(err));
-      },
-      getMessage: async () => {
-        let BACKEND_URL = process.env.BACKEND_URL;
-        fetch(`${BACKEND_URL}/people`)
-          .then((resp) => resp.json())
-          .then((resp) => setStore({ characters: resp.results }))
-          .catch((err) => console.error(err));
+      // getCharacter: async (uid) => {
+      //   let BACKEND_URL = process.env.BACKEND_URL;
+      //   fetch(`${BACKEND_URL}${uid}`)
+      //     .then((resp) => resp.json())
+      //     .then((resp) => setStore({ character: resp.result.properties }))
+      //     .catch((err) => console.error(err));
+      // },
+      // getPlanet: async (uid) => {
+      //   let BACKEND_URL = process.env.BACKEND_URL;
+      //   fetch(`${BACKEND_URL}${uid}`)
+      //     .then((resp) => resp.json())
+      //     .then((resp) => setStore({ planet: resp.result.properties }))
+      //     .catch((err) => console.error(err));
+      // },
+      // getSpaceship: async (uid) => {
+      //   let BACKEND_URL = process.env.BACKEND_URL;
+      //   fetch(`${BACKEND_URL}${uid}`)
+      //     .then((resp) => resp.json())
+      //     .then((resp) => setStore({ vehicle: resp.result.properties }))
+      //     .catch((err) => console.error(err));
+      // },
+      // getMessage: async () => {
+      //   let BACKEND_URL = process.env.BACKEND_URL;
+      //   fetch(`${BACKEND_URL}/people`)
+      //     .then((resp) => resp.json())
+      //     .then((resp) => setStore({ characters: resp.results }))
+      //     .catch((err) => console.error(err));
 
-        fetch(`${BACKEND_URL}/planets`)
-          .then((resp) => resp.json())
-          .then((resp) => setStore({ planets: resp.results }))
-          .catch((err) => console.error(err));
+      //   fetch(`${BACKEND_URL}/planets`)
+      //     .then((resp) => resp.json())
+      //     .then((resp) => setStore({ planets: resp.results }))
+      //     .catch((err) => console.error(err));
 
-        fetch(`${BACKEND_URL}/vehicles`)
-          .then((resp) => resp.json())
-          .then((resp) => setStore({ vehicles: resp.results }))
-          .catch((err) => console.error(err));
-      },
-      getCategories: async () => {
-        let BACKEND_URL = process.env.BACKEND_URL;
-        fetch(`${BACKEND_URL}/categories`)
-          .then((resp) => resp.json())
-          .then((resp) => setStore({ categories: resp.result.properties }))
-          .catch((err) => console.error(err));
-      }
+      //   fetch(`${BACKEND_URL}/vehicles`)
+      //     .then((resp) => resp.json())
+      //     .then((resp) => setStore({ vehicles: resp.results }))
+      //     .catch((err) => console.error(err));
+      // },
     },
   };
 };

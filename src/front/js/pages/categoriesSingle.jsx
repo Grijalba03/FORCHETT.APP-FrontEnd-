ed27493287cd593b29c7bb19e801d09cbb35db77
moniasuka <moniasuka@hotmail.com>
2022-12-06 02:54:06 +0000

    agregando estilo al single view de categories y recipes

--- src/front/js/pages/categoriesSingle.jsx
+++ src/front/js/pages/categoriesSingle.jsx
@@ -6,7 +6,7 @@ import { Context } from "../store/appContext";
 export const Categoriessingle = (props) => {
   const { store, actions } = useContext(Context); //nos traemos las categorias desde store destructurado
   const params = useParams();
-  const [singlecat, setSinglecat] = useState("")
+  const [singlecat, setSinglecat] = useState("");
 
   useEffect(() => {
     async function fetchsingleCategory() {
@@ -38,32 +38,86 @@ export const Categoriessingle = (props) => {
 
   return (
     <>
-    <div className="mt-5">
-      <div className="container spacing">
-        {singlecat ? (
+      <div className="mt-5">
+        <div className="container spacing">
+          {singlecat ? (
+            <div>
+              <h1 className="display-4">
+                {singlecat.category_name ? singlecat.category_name : "Loading"}
+              </h1>
+              <div className="container spacing">
+                <div className="d-flex mt-3 mb-3">
+                  {singlecat.found.map((item, index) => {
+                    return (
+                      // <div key={index}>{item.title}</div>
+
+                      <div key={index} className="col mx-1 px-1">
+                        <div
+                          className="card card-background"
+                          style={{ width: "18rem" }}
+                        >
+                          <div>
+                            {/* Cajita con imagen y rating */}
+                            <img
+                              src="https://via.placeholder.com/400x200"
+                              className="card-img-top rounded"
+                              alt="..."
+                            />
+                            <p>{item.rating}</p>
+                          </div>
+                          <div className="card-body card-background">
+                            {/* Cajita con titulo, descripcion y view / category / like */}
+                            <h5 className="card-title">{item.title}</h5>
+                            <p>{item.carbs}</p>
+                            <p>{item.fat}</p>
+                            <p>{item.free_of}</p>
+                            <p>{item.protein}</p>
+                            <p>{item.servings}</p>
+                            <p>{item.preparation}</p>
+                          </div>
+                          <div className="d-flex justify-content-between mx-3 my-2 card-background">
+                            <div className="align-items-center">
+                              <Link
+                                className="recipe-buttons px-5"
+                                to={`/recipes/${item.id}`}
+                              >
+                                CLICK TO VIEW
+                              </Link>
+                            </div>
+
+                            <button
+                              onClick={(e) => {
+                                actions.addfavorites(item.id);
+                              }}
+                              className="heart align-items-end"
+                            >
+                              <i className="far fa-heart"></i>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          ) : (
+            <h1 className="text-center">No categories available</h1>
+          )}
+
           <div>
-            <h1 className="display-4">{singlecat.category_name ? singlecat.category_name : "Loading"}</h1>
-            <p>{singlecat.found.map((item, index) => {
-              return(
-                <div key={index}>{item.title}</div>
-
-              )
-            })} </p>
-            
+            <Link to="/">
+              <span
+                className="btn btn-primary btn-lg align-items-center"
+                href="#"
+                role="button"
+              >
+                Return to Home
+              </span>
+            </Link>
           </div>
-        ) : (
-          <h1 className="text-center">No categories available</h1>
-        )}
-
-        <div>
-          <Link to="/">
-            <span className="btn btn-primary btn-lg align-items-center" href="#" role="button">
-              Return to Home
-            </span>
-          </Link>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
--- src/front/js/pages/recipeSingle.jsx
+++ src/front/js/pages/recipeSingle.jsx
@@ -6,7 +6,7 @@ import { Context } from "../store/appContext";
 export const Recipesingle = (props) => {
   const { store, actions } = useContext(Context);
   const params = useParams();
-  const [single, setSingle] = useState("")
+  const [single, setSingle] = useState("");
 
   useEffect(() => {
     async function fetchsingleRecipe() {
@@ -28,18 +28,53 @@ export const Recipesingle = (props) => {
       <div className="mt-5">
         <div className="container spacing">
           {single ? (
-            <div>
-              <h1 className="display-4">Title: {single.title ? single.title : "Loading"}</h1>
-              <p className="display-5">Category: {single.category ? single.category : "Loading"}</p>
-              <p className="display-5">Rating: {single.rating ? single.rating : "Loading"}</p>
-              <p className="display-5">Description: {single.description ? single.description : "Loading"}</p>
-              <p className="display-5">Fat: {single.fat ? single.fat : "Loading"}</p>
-              <p className="display-5">Protein: {single.protein ? single.protein : "Loading"}</p>
-              <p className="display-5">Free of: {single.free_of ? single.free_of : "Loading"}</p>
-              <p className="display-5">Image: {single.image ? single.image : "Loading"}</p>
-              <p className="display-5">Servings: {single.servings ? single.servings : "Loading"}</p>
-              <p className="display-5">Ingredients: {single.ingredients ? single.ingredients : "Loading"}</p>
-              <p className="display-5">Preparation: {single.preparation ? single.preparation : "Loading"}</p>
+            <div className="container spacing">
+              <div className="d-flex mt-3 mb-3">
+                <div className="col mx-1 px-1">
+                  <div
+                    className="card card-background"
+                    style={{ width: "18rem" }}
+                  >
+                    <div>
+                      {/* Cajita con imagen y rating */}
+                      <img
+                        src="https://via.placeholder.com/400x200"
+                        className="card-img-top rounded"
+                        alt="..."
+                      />
+                      <p>{single.rating}</p>
+                    </div>
+                    <div className="card-body card-background">
+                      {/* Cajita con titulo, descripcion y detalles*/}
+                      <h5 className="card-title">{single.title}</h5>
+                      <p>{single.carbs}</p>
+                      <p>{single.fat}</p>
+                      <p>{single.free_of}</p>
+                      <p>{single.protein}</p>
+                      <p>{single.servings}</p>
+                      <p>{single.preparation}</p>
+                    </div>
+                    <div className="d-flex justify-content-between mx-3 my-2 card-background">
+                      <div className="align-items-center">
+                        <Link
+                          className="recipe-buttons px-5"
+                          to={`/recipes/${single.id}`}
+                        >
+                          CLICK TO VIEW
+                        </Link>
+                      </div>
+                      <button
+                        onClick={(e) => {
+                          actions.addfavorites(item.id);
+                        }}
+                        className="heart align-items-end"
+                      >
+                        <i className="far fa-heart"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           ) : (
             <h1 className="text-center">No recipes available</h1>
@@ -47,7 +82,11 @@ export const Recipesingle = (props) => {
 
           <div>
             <Link to="/">
-              <span className="btn btn-primary btn-lg align-items-center" href="#" role="button">
+              <span
+                className="btn btn-primary btn-lg align-items-center"
+                href="#"
+                role="button"
+              >
                 Return to Home
               </span>
             </Link>

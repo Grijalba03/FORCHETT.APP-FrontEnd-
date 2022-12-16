089e6779bee78d17c61cdadffc583e64e5cb89ce
nunnoit <devit@tuta.io>
2022-12-16 02:29:34 +0000

    search on header

--- src/front/js/component/header.js
+++ src/front/js/component/header.js
@@ -1,28 +1,67 @@
-// import { Link } from "react-router-dom";
-// import { Context } from "../store/appContext";
+//import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 import React, { useState, useEffect, useContext } from "react";
 import logourl from "../../img/forchettapp.png";
 import "../../styles/header.css";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
-  // const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
   // console.log(store.favorites);
+
+  const userSearch = async (e) => {
+    e.preventDefault();
+    console.log("user Submit recipe func");
+
+    const data = new FormData(e.target);
+    let title = data.get("searchQuery");
+    console.log("obj: ", title);
+
+
+    let obj = {
+      title: title,
+    };
+
+    console.log('obj ', obj);
+    
+
+    let response = await actions.fetchProtegido("/search", obj, "POST");
+    console.log("Response: ", response);
+    if (response.status == 200) {
+      let respuestaJson = await response.json();
+      console.log("Search Query : ", respuestaJson);
+    } else {
+      // error
+    }
+  };
+
   return (
     <div className="container-fluid">
       <div className="row p-3 bg-white">
         <div className="col-4">
-        <Link to="/" className="sd-link">
-          <img src={logourl} />
-        </Link>
+          <Link to="/" className="sd-link">
+            <img src={logourl} />
+          </Link>
         </div>
         <div className="col-4">
           <div className="row">
             <div className="col-md-5 mx-auto">
               <div className="input-group">
+              <form
+                onSubmit={(data) => {
+                  userSearch(data);
+                  // history("/");
+                }}
+              >
                 <input
                   className="form-control border-end-0 border"
                   type="search"
+                  name="searchQuery"
+                  // onSubmit={(data) => {
+                  //   userSearch(data);
+                  //   // history("/");
+                  // }}
+                  //onClick={userSearch}
                   // value="search"
                   // id="example-search-input"
                 />
@@ -30,10 +69,15 @@ export const Header = () => {
                   <button
                     className="btn btn-outline-secondary bg-white border-start-0 border-bottom-0 border ms-n5"
                     type="button"
+                    // onClick={(data) => {
+                    //   userSearch(data);
+                    //   // history("/");
+                    // }}
                   >
                     <i className="fa fa-search"></i>
                   </button>
                 </span>
+                </form>
               </div>
             </div>
           </div>

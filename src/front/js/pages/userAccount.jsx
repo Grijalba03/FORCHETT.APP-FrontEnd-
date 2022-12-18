ae67f4c1b63273c64fc9f716f27b42b05d383c9a
nunnoit <devit@tuta.io>
2022-12-18 17:55:47 +0000

    push userAccount

--- src/front/js/pages/userAccount.jsx
+++ src/front/js/pages/userAccount.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/userAccount.css";
 import Swal from "sweetalert2";
-import {AdvancedImage} from '@cloudinary/react';
+import { AdvancedImage } from "@cloudinary/react";
 import { Cloudinary } from "@cloudinary/url-gen";
 
 export const UserAccount = () => {
@@ -16,36 +16,26 @@ export const UserAccount = () => {
 
   const changeUserPassword = async (e) => {
     e.preventDefault();
-    console.log("Entramos en la función de registrar");
+    console.log("Changing user password");
 
     const data = new FormData(e.target);
-    // let email = data.get("email");
     let currentPassword = data.get("current-password");
     let newPassword = data.get("new-password");
     let confirmPassword = data.get("confirm-password");
 
     console.log(currentPassword, newPassword, confirmPassword);
 
-    // let obj = {
-    //   current-password : currentPassword,
-    //   new-password : newPassword,
-    //   confirm-password: confirmPassword
-    // };
-
     let obj = {};
     obj["current-password"] = currentPassword;
     obj["new-password"] = newPassword;
     obj["confirm-password"] = confirmPassword;
 
-    console.log("hola4");
-    // let response = await actions.login(
     let response = await actions.fetchProtegido(
       "/account/password/" + username,
       obj,
       "PUT"
     );
-    console.log("resss: ", response);
-    console.log("36-response: ", response);
+
     if (response.status == 200) {
       let respuestaJson = await response.json();
       console.log("41: ", respuestaJson);
@@ -73,12 +63,76 @@ export const UserAccount = () => {
     //alert(response.token);
   };
 
+  const changeUserProfile = async (e) => {
+    e.preventDefault();
+    console.log("change user profile data");
+
+    const data = new FormData(e.target);
+    let dietaryPreference = data.get("Dietary-Preference");
+    let title = data.get("Title");
+    let description = data.get("Description");
+    let facebook = data.get("Facebook");
+    let instagram = data.get("Instagram");
+    let twitter = data.get("Twitter");
+    let youtube = data.get("Youtube");
+
+    // console.log(currentPassword, newPassword, confirmPassword);
+
+    let obj = {};
+    obj["Dietary-Preference"] = dietaryPreference;
+    obj["Title"] = title;
+    obj["Description"] = description;
+    obj["Facebook"] = facebook;
+    obj["Instagram"] = instagram;
+    obj["Twitter"] = twitter;
+    obj["Youtube"] = youtube;
+
+    console.log("hola4");
+    // let response = await actions.login(
+    let response = await actions.fetchProtegido(
+      "/account/update/" + username,
+      obj,
+      "PUT"
+    );
+    console.log("resss: ", response);
+    console.log("36-response: ", response);
+    if (response.status == 200) {
+      let respuestaJson = await response.json();
+      console.log("41: ", respuestaJson);
+      Swal.fire({
+        icon: "success",
+        title: "Yes!",
+        text: `Data changed successfully`,
+        footer: "Frack Yeah!",
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "fronk :(",
+        footer: "#GG nothing to do for now",
+      });
+    }
+    //console.log(response);
+    //response = await response.json(); //response es un objeto de Javascript
+    //console.table("hola37", response);
+    //token = response.token;
+    setToken(response.token);
+    console.log("token", token);
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-12 avatarHead">
           <Link to="/uploadCloudinary">
-            <button className="btn btn-outline-info categoryname home my-5" role="button"> Submit Image</button>
+            <button
+              className="btn btn-outline-info categoryname home my-5"
+              role="button"
+            >
+              {" "}
+              Submit Image
+            </button>
           </Link>
           {/* <img
             className="avatarMod d-inline-block"
@@ -163,7 +217,7 @@ export const UserAccount = () => {
 
           <form
             onSubmit={(data2) => {
-              update(data2);
+              changeUserProfile(data2);
             }}
           >
             <div className="row">
@@ -173,7 +227,7 @@ export const UserAccount = () => {
                 </div>
                 <div className="row">
                   <input
-                    name="text"
+                    name="Dietary-Preference"
                     placeholder="Dietary Preference"
                     type="text"
                   />
@@ -184,15 +238,19 @@ export const UserAccount = () => {
                   <h1>User title</h1>
                 </div>
                 <div className="row">
-                  <input name="text" placeholder="User title" type="text" />
+                  <input name="Title" placeholder="User title" type="text" />
                 </div>
               </div>
               <div className="col-12">
                 <div className="row">
-                  <h1>Description</h1>
+                  <h1>Description(Bio)</h1>
                 </div>
                 <div className="row">
-                  <input name="text" placeholder="Description" type="text" />
+                  <input
+                    name="Description"
+                    placeholder="Description"
+                    type="text"
+                  />
                 </div>
               </div>
               <div className="col-12">
@@ -200,7 +258,7 @@ export const UserAccount = () => {
                   <h1>Meta</h1>
                 </div>
                 <div className="row">
-                  <input name="text" placeholder="Meta" type="text" />
+                  <input name="Facebook" placeholder="Meta" type="text" />
                 </div>
               </div>
               <div className="col-12">
@@ -208,7 +266,7 @@ export const UserAccount = () => {
                   <h1>Instagram</h1>
                 </div>
                 <div className="row">
-                  <input name="text" placeholder="Instagram" type="text" />
+                  <input name="Instagram" placeholder="Instagram" type="text" />
                 </div>
               </div>
               <div className="col-12">
@@ -216,7 +274,7 @@ export const UserAccount = () => {
                   <h1>Twitter</h1>
                 </div>
                 <div className="row">
-                  <input name="text" placeholder="Twitter" type="text" />
+                  <input name="Twitter" placeholder="Twitter" type="text" />
                 </div>
               </div>
               <div className="col-12">
@@ -224,7 +282,7 @@ export const UserAccount = () => {
                   <h1>Youtube</h1>
                 </div>
                 <div className="row">
-                  <input name="text" placeholder="Youtube" type="text" />
+                  <input name="Youtube" placeholder="Youtube" type="text" />
                 </div>
               </div>
             </div>

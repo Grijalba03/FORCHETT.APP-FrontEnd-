2f9681e6957229b1ad26d13d5ba7f1aabeb8799f
Grijalba03 <arieswolve26@gmail.com>
2022-12-20 00:17:49 -0600

    favorites sweet functioning AT LAST

--- src/front/js/pages/userProfile.jsx
+++ src/front/js/pages/userProfile.jsx
@@ -6,21 +6,42 @@ import { userActions } from "../store/user";
 export const UserProfile = () => {
   const { store, actions } = useContext(Context);
   const [recarga, setRecarga] = useState(false);
-  const history = useNavigate();
+  const history = useNavigate(); 
+  const [userInfo, setuserInfo] = useState ([]);
+  const [Recipes, setRecipes] = useState ([]);
 
   useEffect(() => {
-    async function fetchData() {
-      let response = await actions.fetchGenerico("/profile/<string:username>");
-      if (response.status == 200) {
-        response = await response.json();
-        store.UserProfile = response;
-        console.log("user profile response",response)
+  async function fetchUserInfo() {
+      let userInfo = await actions.fetchProtegido(`/profile/`+store.username);
+      if (userInfo.status == 200) {
+        userInfo = await userInfo.json();
+        console.log("prueba",userInfo);
+        setuserInfo(userInfo);
+        store.userDetails = userInfo;
+        
+        return userInfo
       } else {
-        response = await response.json();
+        userInfo = await userInfo.json();
+        console.log("si funco", userInfo)
       }
     }
+    fetchUserInfo(); 
 
-    fetchData();
+    async function fetchRecipes() {
+      let Recipes = await actions.fetchProtegido(`/profile/`+store.recipes);
+      if (Recipes.status == 200) {
+        Recipes = await Recipes.json();
+        console.log("prueba",Recipes);
+        setinfoRecipes(Recipes);
+        store.Recipes = Recipes;
+        
+        return Recipes
+      } else {
+        Recipes= await Recipes.json();
+        console.log("si funco", Recipes)
+      }
+    }
+    fetchUserInfo();
 
   }, [recarga]);
 
@@ -29,31 +50,18 @@ export const UserProfile = () => {
       <div className="container">
         <div className="profilebg mx-auto">
           <img
-            src="https://starwars-visualguide.com/assets/img/characters/1.jpg"
+            src={userInfo.image}
             className="profile-img-top rounded-circle mx-auto d-block"
             alt="..."
           /> 
-          <h5 className="username text-center">${user.username}</h5> 
+          <h5 className="username text-center">{store.username}</h5> 
                
           {/*Username and social media links */}
-          <button
-            type="button"
-            className=" buttons under username btn btn-secondary btn-sm"
-          >
-            Youtube
-          </button>{" "}
-          <button
-            type="button"
-            className=" buttons under username btn btn-secondary btn-sm"
-          >
-            Facebook
-          </button>
-          <button
-            type="button"
-            className=" buttons under username btn btn-secondary btn-sm"
-          >
-            Twitter
-          </button>
+          
+          <a className="btn btn-primary" href={userInfo.youtube}  role="button">YouTube</a>
+          <a className="btn btn-primary" href={userInfo.meta}  role="button">Meta</a>
+          <a className="btn btn-primary" href={userInfo.instagram}  role="button">Instagram</a>
+          <a className="btn btn-primary" href={userInfo.twitter}  role="button">Twitter</a>
         </div>
         <div className="favoritecards mx-auto">
           <h3 className="myfavoritestitle text-center">
@@ -61,44 +69,54 @@ export const UserProfile = () => {
             COOKING RECIPES BY USERNAME
           </h3>
         </div>
-        <div className="row row-cols-1 row-cols-md-3 g-4 ">
-          <div className="col">
-            <div className="card-1 h-300">
-              <img
-                src="https://starwars-visualguide.com/assets/img/characters/1.jpg"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Drinks</h5>
-              </div>
+        <div className="container spacing">
+            <div className="d-flex mt-3 mb-3">
+              {store.recipes
+                .filter((auxiliar) => auxiliar.username == store.username)
+                .map((filteredAuxiliar) => (
+                  <div className="col mx-1 px-1">
+                    <div
+                      className="card card-background"
+                      style={{ width: "18rem" }}
+                    >
+                      <div>
+                        {/* Cajita con imagen y rating */}
+                        <img
+                          src={filteredAuxiliar.image}
+                          className="card-img-top rounded"
+                          alt="..."
+                        />
+                        <p>{filteredAuxiliar.rating}</p>
+                      </div>
+                      <div className="card-body card-background">
+                        {/* Cajita con titulo, descripcion y view / category / like */}
+                        <h5 className="card-title">{filteredAuxiliar.title}</h5>
+                        <p>{filteredAuxiliar.description}</p>
+                      </div>
+                      <div className="d-flex justify-content-between mx-3 my-2 card-background">
+                        <div className="align-items-center">
+                          <Link
+                            className="recipe-buttons px-5"
+                            to={`/recipes/${filteredAuxiliar.id}`}
+                          >
+                            CLICK TO VIEW
+                          </Link>
+                        </div>
+
+                        <button
+                          onClick={(e) => {
+                            actions.addFav(filteredAuxiliar.title);
+                          }}
+                          className="heart align-items-end"
+                        >
+                          <i className="far fa-heart"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
             </div>
           </div>
-          <div className="col">
-            <div className="card-2 h-300">
-              <img
-                src="https://starwars-visualguide.com/assets/img/characters/2.jpg"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Breakfast</h5>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card-3 h-300">
-              <img
-                src="https://starwars-visualguide.com/assets/img/characters/14.jpg"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Brunch</h5>
-              </div>
-            </div>
-          </div>
-        </div>
 
         <div className="row justify-content-center">            
             <div className="col"></div>      

65f51e8dcf207c3e1b4c33e0b91b2644e20e2411
moniasuka <moniasuka@hotmail.com>
2022-12-21 21:25:58 +0000

    actualizando view de favorites

--- src/front/js/pages/favorites.js
+++ src/front/js/pages/favorites.js
@@ -45,41 +45,67 @@ export const Favorites = () => {
 
   return (
     <>
-      <div className="container">
+      <div className="container my-5">
         <div className="profilebg mx-auto">
           <img
             src={userInfo.image}
             className="profile-img-top rounded-circle mx-auto d-block"
             alt="..."
           />
-          <h5 className="username text-center">{store.username}</h5>
-          <h6 className="myprofile text-center"> My Profile</h6>
+          <h2 className="text-center userfont">{store.username}</h2>
         </div>
         <div className="favoritecards mx-auto">
           <h3 className="myfavoritestitle text-center"> MY FAVORITES</h3>
         </div>
-        <div className="container spacing">
+
+
         {favList && favList.length > 0 ? (
-          favList.map((item, index) => {
-            return (
-              <li className="ms-1 me-1 text-primary" key={index}>
-                {item.recipe_title}
-                <i
-                  onClick={() => actions.removeFav(item.id)}
-                  class="fa fa-trash"
-                  aria-hidden="true"
-                  role="button"
-                ></i>
-              </li>
-            );
-          })
-        ) : (
-          <>
-            <p> NO hay favoritos </p>
-          </>
-        )}
+          <div className="container spacing">
+              <div className="row d-flex mt-3 mb-3">
+                {favList.map((item, index) => {
+                  return (
+                    <div key={index} className="col mx-1 px-1 my-2 py-1">
+                      <div
+                        className="card card-background"
+                        style={{ width: "18rem" }}
+                      >
+                        <div classname="containingimage">
+                          <img
+                            src={item.recipe_image}
+                            className="card-img-top rounded"
+                            alt="..."
+                          />
+                        </div>
+                        <div className="card-body card-background">
+                          <h5 className="card-title text-center">
+                            {item.recipe_title}
+                          </h5>
+                        </div>
+                        <div className="d-flex justify-content-between mx-2 my-2 card-background">
+                          <div>
+                            <Link
+                              className="recipe-buttons"
+                              to={`/recipes/${item.id}`}
+                            >
+                              VIEW
+                            </Link>
+                          </div>
+                          <div className="ms-1 me-1">
+                            <i onClick={() => actions.removeFav(item.id)}
+                              className="fa fa-trash basurero" aria-hidden="true" role="button"
+                            ></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+        ) : <><p className="text-center tipofav my-5"><i className="fa-solid fa-triangle-exclamation"></i> No favorites to display</p> </>}
           
-        </div>
+       
+
         <div className="row justify-content-center">
           <div className="col"></div>
           <div className="col mx-auto align-items-middle">
